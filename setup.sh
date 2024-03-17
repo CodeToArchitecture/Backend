13f0a760700456c145402a635fb57ed06ecfc984
akuld2 <akuldatta@gmail.com>
2024-03-16 20:11:31 -0700

    t

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-npm install express github-repo-parser axios dotenv json_server archiver draw.io-export drawio
+npm install express github-repo-parser axios dotenv archiver draw.io-export drawio
 npm install @anthropic-ai/sdk
 mkdir overview_files/
 touch login.txt
